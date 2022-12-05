d2716b24b65ce8b98d5793eba272fb9dceb6815d
Thales Menezes <thlmenezes@pm.me>
2022-12-04 22:58:26 -0300

    🔨 dev env tmt

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-};
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['cmyk', 'business'],
+    darkTheme: 'business',
+  },
+}
