4c0661d40f4470200162205b28bb2818225e2471
Thales Menezes <thlmenezes@pm.me>
2022-12-16 22:04:40 -0300

    🐛 🔥 remove example router mentions

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,11 +1,9 @@
 import { router } from '../trpc'
 
 import { authRouter } from './auth'
-import { exampleRouter } from './example'
 import { userRouter } from './user'
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
   user: userRouter,
 })
