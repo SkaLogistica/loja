97fed8747d7b81f7f3713b0711df30145e0e35c8
Thales Menezes <thlmenezes@pm.me>
2022-12-21 15:54:01 -0300

    🌐 sign {in,out} button on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ const AuthButton: React.FC = () => {
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
         onClick={sessionData ? () => signOut() : () => signIn('google')}
       >
-        {sessionData ? 'Sign out' : 'Sign in'}
+        {sessionData ? 'SAIR' : 'ENTRAR'}
       </button>
     </div>
   )
