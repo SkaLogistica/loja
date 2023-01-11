35beac14fa52c97c576c3558933fe34ccc9d7cd4
Thales Menezes <thlmenezes@pm.me>
2023-01-11 12:14:11 -0300

    💄 main content rewrite

--- src/pages/[category]/index.tsx
+++ src/pages/[category]/index.tsx
@@ -47,7 +47,7 @@ const CategoryPage: NextPage = () => {
         setName(e.target.value)
       }}
     >
-      <main className="flex w-full flex-1 flex-col items-center justify-center gap-4">
+      <main className="mt-4 flex w-full flex-1 flex-col items-center gap-y-4 md:items-start md:px-9 lg:px-24">
         <div className="breadcrumbs text-sm">
           <ul>
             <li>
@@ -63,88 +63,91 @@ const CategoryPage: NextPage = () => {
           height={400}
           src={categoryBannerUrl}
           alt={`Banner da categoria ${category}`}
+          className="hidden max-h-44 w-full md:block"
         />
-        <div className="grid grid-cols-1 gap-2 lg:grid-cols-2">
-          {productsData?.map((product) => (
-            <div
-              key={product.id}
-              className="card border bg-base-100 shadow-xl lg:card-side"
-            >
-              {product.photos.length > 0 ? (
-                <figure>
-                  <Image
-                    width={256}
-                    height={256}
-                    src={product.photos[0]?.url ?? ''}
-                    alt={`Imagem do ${product.name}`}
-                    className="w-36 object-contain lg:w-64"
-                  />
-                </figure>
-              ) : (
-                <></>
-              )}
-              <div className="card-body">
-                <h2 className="card-title">{product.name}</h2>
-                <p>{currencyFormatter(Number(product.price))}</p>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Comprar</button>
+        <div className="box-border flex w-full flex-col items-center gap-y-4">
+          <div className="grid grid-cols-1 gap-2 lg:grid-cols-2">
+            {productsData?.map((product) => (
+              <div
+                key={product.id}
+                className="card border bg-base-100 shadow-xl lg:card-side"
+              >
+                {product.photos.length > 0 ? (
+                  <figure>
+                    <Image
+                      width={256}
+                      height={256}
+                      src={product.photos[0]?.url ?? ''}
+                      alt={`Imagem do ${product.name}`}
+                      className="aspect-square w-36 object-contain lg:w-64"
+                    />
+                  </figure>
+                ) : (
+                  <></>
+                )}
+                <div className="card-body">
+                  <h2 className="card-title">{product.name}</h2>
+                  <p>{currencyFormatter(Number(product.price))}</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Comprar</button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
-        <div className="btn-group grid grid-cols-3">
-          <button
-            className="btn btn-ghost"
-            disabled={!page}
-            onClick={() => setPage((old) => Math.max(0, old - 1))}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="192"
-              height="192"
-              fill="#000000"
-              viewBox="0 0 256 256"
-              className="h-4 w-4"
-            >
-              <rect width="256" height="256" fill="none"></rect>
-              <polyline
-                points="160 208 80 128 160 48"
-                fill="none"
-                stroke="#000000"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="16"
-              ></polyline>
-            </svg>
-          </button>
-          <div className="flex cursor-not-allowed items-center justify-center bg-primary/30">
-            {page + 1}
+            ))}
           </div>
-          <button
-            className="btn btn-ghost"
-            disabled={disableNextPage}
-            onClick={() => setPage((old) => old + 1)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="192"
-              height="192"
-              fill="#000000"
-              viewBox="0 0 256 256"
-              className="h-4 w-4"
+          <div className="btn-group grid grid-cols-3">
+            <button
+              className="btn btn-ghost"
+              disabled={!page}
+              onClick={() => setPage((old) => Math.max(0, old - 1))}
             >
-              <rect width="256" height="256" fill="none"></rect>
-              <polyline
-                points="96 48 176 128 96 208"
-                fill="none"
-                stroke="#000000"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="16"
-              ></polyline>
-            </svg>
-          </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="192"
+                height="192"
+                fill="#000000"
+                viewBox="0 0 256 256"
+                className="h-4 w-4"
+              >
+                <rect width="256" height="256" fill="none"></rect>
+                <polyline
+                  points="160 208 80 128 160 48"
+                  fill="none"
+                  stroke="#000000"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="16"
+                ></polyline>
+              </svg>
+            </button>
+            <div className="flex cursor-not-allowed items-center justify-center bg-primary/30">
+              {page + 1}
+            </div>
+            <button
+              className="btn btn-ghost"
+              disabled={disableNextPage}
+              onClick={() => setPage((old) => old + 1)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="192"
+                height="192"
+                fill="#000000"
+                viewBox="0 0 256 256"
+                className="h-4 w-4"
+              >
+                <rect width="256" height="256" fill="none"></rect>
+                <polyline
+                  points="96 48 176 128 96 208"
+                  fill="none"
+                  stroke="#000000"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="16"
+                ></polyline>
+              </svg>
+            </button>
+          </div>
         </div>
       </main>
     </StoreLayout>
